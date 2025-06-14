d0bfc4de41106b8613dd87073e94154ac75c2255
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2025-06-14 10:03:36 +0100

    Created util functions for file and dir tasks

---
+++ internal/utils/files.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func FileExists(filePath string) (bool, error) {
+	info, err := os.Stat(filePath)
+	if err == nil {
+		return !info.IsDir(), nil
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	}
+	return false, err
+}
+
+func DirExists(dirPath string) (bool, error) {
+	info, err := os.Stat(dirPath)
+	if err == nil {
+		return info.IsDir(), nil
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	}
+	return false, err
+}
+
+func CreateDir(dirPath string) error {
+	if err := os.MkdirAll(dirPath, 0o700); err != nil {
+		fmt.Fprintf(os.Stderr, "▸ could not create config directory %q: %v\n", dirPath, err)
+		return err
+	}
+	return nil
+}
